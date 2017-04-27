b043f2a8c34168117a28664fccd32ba44e5011f3
tomas <tomas.kraus@icord.cz>
2017-04-27 11:51:41 +0200

    refactor: rename value1

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $("#compute").click(function(){
         // alert("clicked");
 
-        var res = $("#value1").val() * $("#coeff").val();
+        var res = $("#value").val() * $("#coeff").val();
         $("#result").val(res);
         return false;
     });
