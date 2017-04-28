0862af434ea5778b7b15f3987dd9f859e2b68057
tomas <tomas.kraus@icord.cz>
2017-04-28 11:40:50 +0200

    refactor: encapsulate the  converting logic

--- js/script.js
+++ js/script.js
@@ -2,10 +2,19 @@ $(document).ready(function(){
     $("#compute").click(function(){
         // alert("clicked");
 
-        var res = $("#value").val() * $("#coeff").val();
-        var invRes = $("#value").val() / $("#coeff").val();
-        $("#result").val(res);
-        $("#inv-result").val(invRes);
+        $("#result").val(converterObj.convert($("#value").val(), $("#coeff").val()));
+        $("#inv-result").val(converterObj.convertInverse($("#value").val(), $("#coeff").val()));
         return false;
     });
 });
+
+
+var converterObj = {
+    convert : function(value, coeff) {
+        return value * coeff;
+    },
+
+    convertInverse : function(value, coeff) {
+        return value / coeff;
+    }
+}
